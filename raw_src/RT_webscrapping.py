cec625ca057a625a2918ee9949207786319abc52
xin.lei <realleixin@qq.com>
2026-07-16 23:44:12 +0800

    feat: modify source code

--- raw_src/RT_webscrapping.py
+++ raw_src/RT_webscrapping.py
@@ -22,11 +22,18 @@ def web_content_div(web_content, class_path):
     Get the information from the url
 """
 def real_time_price(stock_code):
+    
+    headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
+            'Accept': 'application/json',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Referer': 'https://finance.yahoo.com/'
+        }
     url = f"https://finance.yahoo.com/quote/{stock_code}/"
     try:
-        r = requests.get(url)
+        r = requests.get(url, headers=headers)
         web_content = BeautifulSoup(r.text, "lxml")
-        texts = web_content_div(web_content, "My(6px) Pos(r) smartphone_Mt(6px)")
+        texts = web_content_div(web_content, "container yf-z2uro5")
         
         if texts != []:
             price, change = texts[0], texts[1]
@@ -39,6 +46,6 @@ def real_time_price(stock_code):
     return price, change
 
 
-Stock = ["BRK-B"]
+Stock = ["AAPL"]
 
 print(real_time_price(Stock[0]))
