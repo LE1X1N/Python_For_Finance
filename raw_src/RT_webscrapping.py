e3478e51b322ef71cfadf92989a397d526e8c764
xin.lei <realleixin@qq.com>
2026-07-17 10:18:39 +0800

    doc: add source code for course 2

--- raw_src/RT_webscrapping.py
+++ raw_src/RT_webscrapping.py
@@ -17,6 +17,19 @@ def web_content_div(web_content, class_path):
     
     return texts
 
+def web_content_li(web_content, class_path):
+    web_content_li = web_content.find_all('li', {'class': class_path})
+    fundimentals = {}
+    try:
+        for tag in web_content_li:
+            spans = tag.find_all('span')
+            texts = [span.get_text() for span in spans]
+            fundimentals[str(texts[0]).strip()] = str(texts[1]).strip()
+    except IndexError:
+        texts = []
+    
+    return fundimentals
+
 
 """
     Get the information from the url
@@ -36,16 +49,40 @@ def real_time_price(stock_code):
         texts = web_content_div(web_content, "container yf-z2uro5")
         
         if texts != []:
-            price, change = texts[0], texts[1]
+            price, change = texts[0], texts[1]+texts[2]
         else:
             price, change = [], []
         
-    except ConnectionError:
-        price, change = [], []
+        fundimentals = web_content_li(web_content, "yf-gf32ga")
+        
+        volume = fundimentals["Volume"]
+        one_year_target = fundimentals["1y Target Est"]
     
-    return price, change
+        
+    except ConnectionError:
+        price, change, volume, one_year_target = [], [], [], []
+    
+    return price, change, volume, one_year_target
 
 
-Stock = ["AAPL"]
+Stock = ["AAPL", "BRK-B", "PYPL", "AMZN", "BABA", "MSFT", "GOOG"]
 
-print(real_time_price(Stock[0]))
+while (True):
+    info = []
+    col = []
+    time_stamp = datetime.datetime.now()
+    time_stamp = time_stamp.strftime('%Y-%m-%d %H:%M:%S')
+    
+    for stock_code in Stock:
+        price, change, volume, one_year_target = real_time_price(stock_code)
+        info.append(price)
+        info.extend([change])
+        info.extend([volume])
+        info.extend([one_year_target])
+    
+    col = [time_stamp]
+    col.extend(info)
+    df = pd.DataFrame(col).T
+    df.to_csv(str(time_stamp[0:11])+'stock data.csv', mode='a', header=False)
+    print(col)
+        
