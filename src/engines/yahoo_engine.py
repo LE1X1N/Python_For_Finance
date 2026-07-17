ffbacd7a029d4f6cc3e4b5434feee6ace61a6e3c
xin.lei <realleixin@qq.com>
2026-07-17 08:55:19 +0800

    refactor: add Error capturing

--- src/engines/yahoo_engine.py
+++ src/engines/yahoo_engine.py
@@ -20,22 +20,32 @@ class YahooEngine(BaseStockEngine):
         self.class_path = "container yf-z2uro5"  
     
     def get_real_time_quote(self, stock_code):
+        price, change, rate = -1,-1,-1
+        
         try:
             r = requests.get(self.url+stock_code, headers=self.headers)
+            
+            if r.status_code != 200:
+                raise ConnectionError(f"Statue Code: {r.status_code}")
+            
             web_content = BeautifulSoup(r.text, "lxml")
             web_content_div = web_content.find_all('div', {'class': self.class_path})
             try:
                 spans = web_content_div[0].find_all('span')
                 texts = [span.get_text() for span in spans]
             except IndexError:
-                raise KeyError
+                raise
             
             if texts != []:
                 price, change, rate = texts[0], texts[1], texts[2]
             else:
-                raise KeyError
+                raise ValueError
             
         except ConnectionError:
-            raise KeyError
+            print("网络链接失败，请尝试其他数据源！")
+        except IndexError as e :
+            print(f"页面解析失败, 无法找到div: {self.class_path}" + e)
+        except ValueError:
+            print(f"页面解释失败，在div: {self.class_path}下无法找到可用数据")
         
         return price, change, rate
