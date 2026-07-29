bf254c8a113ccff6ee42afeebb4fec1875a6d420
xin.lei <realleixin@qq.com>
2026-07-29 10:24:37 +0800

    feat: parse stock fundimentals from ul-li rag by selenium

--- part1/Daily_schedule.py
+++ part1/Daily_schedule.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
 
-def xpath_element(xpath):
+def xpath_element(driver, xpath):
     
     try:
         element = driver.find_element(By.XPATH, xpath)
@@ -22,8 +22,9 @@ def real_time_price(stock_code):
     url = f"https://finance.yahoo.com/quote/{stock_code}/"
     driver.get(url)
     
+    # Stock quote
     xpath = '//*[@id="main-content-wrapper"]/section[1]/div[2]/div[1]/section/div/section[1]/div[1]'
-    stock_price_info = xpath_element(xpath).text.split()
+    stock_price_info = xpath_element(driver, xpath).text.split()
     
     if stock_price_info != []:
         price = stock_price_info[0]
@@ -31,8 +32,23 @@ def real_time_price(stock_code):
     else:
         price = []
         change = []
+        
+    # Stock fundimentals
+    xpath = '//*[@id="main-content-wrapper"]/section[3]/div/div/div/ul'
+    li_elements = xpath_element(driver, xpath).find_elements(By.TAG_NAME, "li")
     
-    volume, latest_pattern, one_year_target = [], [], [], [], []
+    if li_elements:
+        fundimental_info = {}
+        for li in li_elements:
+            kv = li.text.split("\n", maxsplit=2)
+            fundimental_info[kv[0]] = kv[1]
+        
+        volume = fundimental_info["Volume"]
+        one_year_target = fundimental_info["1y Target Est"]
+    else:
+        volume = []
+        one_year_target = []
+     
     return price, change, volume, latest_pattern, one_year_target
 
 
