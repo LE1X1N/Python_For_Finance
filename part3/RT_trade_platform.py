417cceb655ee6d3acd7183ae22bcdc06af54914c
xin.lei <realleixin@qq.com>
2026-07-29 11:54:26 +0800

    feat: introduce Part3

---
+++ part3/RT_trade_platform.py
@@ -0,0 +1,56 @@
+import math
+import datetime
+import pandas as pd
+import pandas_ta as ta
+import numpy as np
+
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.transforms as transform
+import matplotlib.ticker as mticker
+from matplotlib.gridspec import GridSpec
+from matplotlib.widgets import CheckButtons
+from mplfinance.original_flavor import candlestick_ohlc
+
+def figure_design():
+    pass
+
+def plot_volume():
+    pass
+
+def plot_MACD():
+    pass
+
+def plot_RSI():
+    pass
+
+def plot_x_axis_time():
+    pass
+
+def process_data():
+    pass
+
+def interactive_TA():
+    pass
+
+def interactive_strategy():
+    pass
+
+def animate():
+    pass
+
+fig = plt.figure()
+fig.patch.set_facecolor('#121416')
+gs = fig.add_gridspec(10, 6)
+ax1 = fig.add_subplot(gs[0:7, 0:6])
+ax2 = fig.add_subplot(gs[7, 0:6])
+ax3 = fig.add_subplot(gs[8, 0:6])
+ax4 = fig.add_subplot(gs[9, 0:6])
+figure_design([ax1, ax2, ax3, ax4])
+
+Stock = ['AAPL']
+plot_button_TA = interactive_TA()
+plot_button_strategy = interactive_strategy()
+
+ani = animation.FuncAnimation(fig, animate, interval=1)
+plt.show()
